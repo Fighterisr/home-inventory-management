bd2fff08e926c62e21c791fe1960518e27af9c29
Omri Biton <fighterisr@gmail.com>
2022-03-14 01:06:04 +0200

    styled FilterList

--- src/components/FilterList.js
+++ src/components/FilterList.js
@@ -1,6 +1,47 @@
-import {TextField} from "@mui/material";
+import {alpha, InputBase, styled} from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 import {useDispatch} from "react-redux";
 import {filterActions} from "../store/filter-slice";
+
+const Search = styled('div')(({theme}) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({theme}) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({theme}) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+}));
 
 
 const FilterList = () => {
@@ -11,7 +52,16 @@ const FilterList = () => {
     }
 
     return (
-        <TextField sx={{bgcolor: 'white'}} variant="filled" onChange={inputHandler}/>
+        <Search>
+            <SearchIconWrapper>
+                <SearchIcon/>
+            </SearchIconWrapper>
+            <StyledInputBase
+                placeholder="Search…"
+                inputProps={{'aria-label': 'search'}}
+                onChange={inputHandler}
+            />
+        </Search>
     )
 }
 
